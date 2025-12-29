8a835e81c95ab2623377da2d5da240f79f675549
ms200788 <ms97179129@gmail.com>
2025-12-29 13:00:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -597,13 +597,12 @@ def users(m):
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "OK"
-
+@app.route('/health')
+def health():
+    return "OK", 200
 
 def run_web():
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "10000")))
 
 
 # ============================================================
